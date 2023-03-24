175684f421677f9dd375da3e4e3a3d00bb4d0b95
Joshimello <joshualeanjw@gmail.com>
2023-03-24 12:14:38 +0800

    cd

--- utils/toJson.js
+++ utils/toJson.js
@@ -1,10 +1,10 @@
 import fs from 'fs'
 import { parse } from 'yaml'
 
-let files = fs.readdirSync('../games/')
+let files = fs.readdirSync('./games/')
 
 let data = files
   .map(file => parse(fs.readFileSync('./games/' + file, 'utf8')))
   .sort((a, b) => a.game > b.game ? 1 : -1)
 
-fs.writeFileSync('../src/games.json', JSON.stringify(data))
+fs.writeFileSync('./src/games.json', JSON.stringify(data))
