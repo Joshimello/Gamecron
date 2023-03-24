9b6d623d722f345544cb8170546eae49cbf6617f
Joshimello <joshualeanjw@gmail.com>
2023-03-24 11:43:31 +0800

    search

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,19 @@
 <script>
+  import MiniSearch from 'minisearch'
   import { onMount } from 'svelte'
   import '@fontsource/roboto-slab'
   import '@fontsource/roboto-mono'
 
   let games = []
+  let value = ''
+  let minisearch
 
   onMount( async() => {
 
     let data = (await import('./games.json')).default
 
     setInterval(() => {
-      games = data.map(game => {
+      games = data.map((game, index) => {
 
         game['serverTime'] = new Date(new Date().toLocaleString('en-US', { timeZone: game.timezone }))
 
@@ -31,36 +34,56 @@
 
       })
     }, 1000)
+
+    minisearch = new MiniSearch({
+      fields: ['game'],
+      storeFields: ['id'],
+      searchOptions: {
+        fuzzy: 1
+      }
+    })
+
+    minisearch.addAll(data.map((game, index) => ({...game, id: index})))
+
   })
+
+  $: searchResultIDs = minisearch ? minisearch.search(value) : []
+  $: searchResult = searchResultIDs.length ? searchResultIDs.map(entry => games[entry.id]) : games
     
 </script>
 
-<div class="bg-gray-100 min-h-100vh p-8 flex flex-col gap-4">
-  {#if games.length}
-    {#each games as game}
-      <div class="bg-white shadow-xl p-4 rounded flex flex-row gap-4">
+<main class="bg-gray-100 h-100vh flex flex-col items-center overflow-y-scroll">
 
-        <img class="h-16 rounded-2xl" src="./icons/{game.game.replace(/[/\\?%*:|"<>]/g, '').toLowerCase()}.webp" alt={game.game} />
+  <div class="px-8 flex flex-col gap-4 w-160">
+    {#if games.length}
 
-        <div class="flex flex-col flex-1">
-          <div class="flex justify-between items-start">
-            <span class="text-2xl font-serif">{game.game}</span>
-            <span class="text-sm font-mono">{game.server.toUpperCase()}</span>
+      <input class="bg-white shadow-xl px-8 py-4 my-4 text-xl outline-0" bind:value placeholder="Search..." />
+
+      {#each searchResult as game}
+        <div class="bg-white shadow-xl p-4 rounded flex flex-row gap-4">
+
+          <img class="h-16 rounded-2xl" src="./icons/{game.game.replace(/[/\\?%*:|"<>]/g, '').toLowerCase()}.webp" alt={game.game} />
+
+          <div class="flex flex-col flex-1">
+            <div class="flex justify-between items-start">
+              <span class="text-2xl font-serif">{game.game}</span>
+              <span class="text-sm font-mono">{game.server.toUpperCase()}</span>
+            </div>
+            <span class="font-mono text-lg">
+              {String(game.timeToReset[0]).padStart(2, '0')}h
+              {String(game.timeToReset[1]).padStart(2, '0')}m
+              {String(game.timeToReset[2]).padStart(2, '0')}s
+            </span>
+            <div class="border-orange-100 border-3 rounded" style="width: {game.percentToReset}%"></div>
           </div>
-          <span class="font-mono text-lg">
-            {String(game.timeToReset[0]).padStart(2, '0')}h
-            {String(game.timeToReset[1]).padStart(2, '0')}m
-            {String(game.timeToReset[2]).padStart(2, '0')}s
-          </span>
-          <div class="border-orange-100 border-3 rounded" style="width: {game.percentToReset}%"></div>
-        </div>
 
-      </div>
-    {/each}
-  {:else}
-    Loading...
-  {/if}
-</div>
+        </div>
+      {/each}
+    {:else}
+      Loading...
+    {/if}
+  </div>
+</main>
 
 <style>
   .font-serif { font-family: 'Roboto Slab', serif; }
